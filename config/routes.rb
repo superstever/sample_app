04809d52a491da2c04aaacf2bf06871fb064ff02
Stephen Lee <superstever@gmail.com>
2010-04-30 20:12:08 -0400

    used rail routes for URLs and removed public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,13 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  # Routes for sample_app website here START
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.root                :controller => 'pages', :action => 'home'
+  # Routes for sample_app website END
+ 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
