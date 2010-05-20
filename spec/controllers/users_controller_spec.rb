e7db51e78ad406e0d807787ac28cd4040e3dd069
Stephen Lee <superstever@gmail.com>
2010-05-20 17:38:53 -0400

    modified app view layouts applicationhtmlerb to use content_tag. Added several tests to test each field on signup sheet.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,10 +38,31 @@ describe UsersController do
       response.should be_success
     end
     
-  it "should have the right title" do
-    get :new
-    response.should have_tag("title", /Sign up/)
+    it "should have the right title" do
+      get :new
+      response.should have_tag("title", /Sign up/)
     end  
+    
+    it "should have a name field" do
+      get :new
+      response.should have_tag("input[name=?][type=?]","user[name]", "text")
+    end
+    
+    it "should have an email field" do
+      get :new
+      response.should have_tag("input[name=?][type=?]","user[email]", "text")
+    end
+    
+    it "should have a password field" do
+      get :new
+      response.should have_tag("input[name=?][type=?]","user[password]", "password")
+    end
+
+    it "should have a password confirmation field" do
+       get :new
+       response.should have_tag("input[name=?][type=?]","user[password_confirmation]", "password")
+     end 
+    
   end #End "Get 'new'"
   
   describe "POST 'create'" do
