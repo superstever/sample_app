016fd50c550b147124cdce35c51ee2ddfaecfafa
Stephen Lee <superstever@gmail.com>
2010-06-03 16:12:57 -0400

    finished ch9 exercises 1 and 2

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -45,16 +45,31 @@ describe "Users" do
   
     describe "success" do
       it "should sign a user in and out" do
-        user = Factory(:user)
-        visit signin_path
-        fill_in   :email,     :with => user.email
-        fill_in   :password,  :with => user.password
-        click_button
+       user = Factory(:user)
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link  "Sign out"
         controller.should_not be_signed_in
       end
     end #end "success" (from <sign in/out>)
   end #end "sign in/out"
+  
+
+  describe "sign in/out/in" do
+    describe "failure" do
+      it "should not fail sign-in again after I sign out" do
+       user = Factory(:user)
+       integration_sign_in(user)
+       controller.should be_signed_in
+       click_link  "Sign out"
+       controller.should_not be_signed_in
+       click_link "Sign in"
+       fill_in :email, :with => user.email
+       fill_in :password, :with => user.password
+       click_button
+       controller.should be_signed_in
+      end
+    end #end "failure" (from "sign in/out/in")
+  end #end "sign in/out/in"
   
 end #end "Users"
