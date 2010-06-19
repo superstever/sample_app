3400ee20570a7570fe571a4f39b4ddff5428f5cd
Stephen Lee <superstever@gmail.com>
2010-06-19 09:09:51 -0400

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  #need :only => [:edit, :update] which limits it to these two functions
+  #if you don't include, you :authenticate for all functions in UsersController before
+  #they run. This violates the expectations in user_controller_spec and makes 
+  #every test fail. 
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
 
   def show
     @user = User.find(params[:id])
@@ -22,5 +29,44 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
+  private
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 
 end
